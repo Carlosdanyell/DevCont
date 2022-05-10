bb87469acc289e33f24209d3f1c212dfceee7546
carlosdanyell <carlosdanyellnsb@hotmail.com>
2022-05-10 20:58:30 -0300

    finalização da estrutura e estilo da pagina de calculadora, e começo da página principal

--- scripts/scriptElementsPage.js
+++ scripts/scriptElementsPage.js
@@ -1,14 +1,23 @@
 const btnMenu = document.getElementById('btn-menu-mobile');
 const navigation = document.getElementById('menu');
-const headerPage = document.getElementById('center');
+const body = document.querySelector('body');
+const header = document.querySelector('nav');
 
 function toggleMenu (event){
 
  btnMenu.classList.toggle('activebtn')
  navigation.classList.toggle('active')
- headerPage.classList.toggle('active')
+ body.classList.toggle('hidden')
 
+}
+function onScroll(){
+    if(scrollY > 0){
+        header.classList.add('scroll');
+    }else{
+        header.classList.remove('scroll');
+    }
 }
 
 
+window.addEventListener('scroll', onScroll)
 btnMenu.addEventListener('click', toggleMenu);
